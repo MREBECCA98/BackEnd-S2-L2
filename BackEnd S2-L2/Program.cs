74c46883f7b4e238bdbbb8f11581c068816533d3
MREBECCA98 <reb.matarozzo@gmail.com>
2025-11-11 17:09:13 +0100

    controlli info personali

--- BackEnd S2-L2/Program.cs
+++ BackEnd S2-L2/Program.cs
@@ -31,17 +31,52 @@ switch (scelta)
                 }
             } while (string.IsNullOrWhiteSpace(cv.InfoPersonali.Nome));
 
+            //cognome
+            do
+            {
+                Console.WriteLine("\nCognome:");
+                cv.InfoPersonali.Cognome = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(cv.InfoPersonali.Cognome))
+                {
+                    Console.WriteLine("Campo vuoto, inserisci il tuo cognome");
+                }
+            } while (string.IsNullOrWhiteSpace(cv.InfoPersonali.Cognome));
+
+            //telefono
+            do
+            {
+                Console.WriteLine("\nTelefono:");
+                cv.InfoPersonali.Telefono = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(cv.InfoPersonali.Telefono))
+                {
+                    Console.WriteLine("Campo vuoto, inserisci il tuo numero di telefono");
+                }
+                else if (cv.InfoPersonali.Telefono.Length != 10)
+                {
+                    Console.WriteLine("Numero di telefono non valido");
+                }
+            } while (string.IsNullOrWhiteSpace(cv.InfoPersonali.Telefono) || cv.InfoPersonali.Telefono.Length != 10);
+
+            //Email
+            do
+            {
+                Console.WriteLine("\nEmail:");
+                cv.InfoPersonali.Email = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(cv.InfoPersonali.Email))
+                {
+                    Console.WriteLine("Campo vuoto, inserisci il tuo nome");
+                }
+                else if (!cv.InfoPersonali.Email.Contains("@"))
+                {
+                    Console.WriteLine("Campo non valido, inserisci @");
+                } ;
+            } while (string.IsNullOrWhiteSpace(cv.InfoPersonali.Email) || !cv.InfoPersonali.Email.Contains("@"));
 
 
 
-            Console.WriteLine("\nCognome:");
-            cv.InfoPersonali.Cognome = Console.ReadLine();
-
-            Console.WriteLine("\nTelefono:");
-            cv.InfoPersonali.Telefono = Console.ReadLine();
-
-            Console.WriteLine("\nEmail:");
-            cv.InfoPersonali.Email = Console.ReadLine();
 
             Info.Add(cv);
         }
